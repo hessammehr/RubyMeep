d186d46ae36c7ff1fad67591e7680b6ad40a3490
S. Hessam Moosavi Mehr <hessammehr@gmail.com>
2010-11-05 19:54:21 +0330

    A few edits here and there.

--- simulation.rb
+++ simulation.rb
@@ -4,18 +4,22 @@ require 'pml'
 
 module MEEP
   class Simulation
-    attr_accessor :geometry, :sources
+    attr_accessor :geometry, :sources, :resolution, :size, 
     Defaults = {'progress-interval' => 40, 'resolution' => 5}
-    def initialize(geometry, sources, options)
+    def initialize
       @geometry = geometry
       @sources = sources
       @options = Defaults.merge options
     end
     def to_s
-      resolution = "(set! resolution #{options['resolution']})"
-      geometry = @geometry.to_s
-      sources = @sources.to_s
+      resolution_str = "(set! resolution #{options['resolution']})"
+      geometry = @geometry.join("\n")
+      geometry_str = "(set! geometry \n (list\n#{geometry}))"
+      sources = @sources.join("\n")
       
+    end
+    def sources source_list
+      @sources = source_list
     end
   end
 end
